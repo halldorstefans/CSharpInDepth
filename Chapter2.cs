f447fcb3eeba732bf4e46366e21c325854fde9b6
Halldor Stefansson <halldor@pop-os.localdomain>
2020-08-10 22:13:23 +0100

    Initial commit

---
+++ Chapter2.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpInDepth
+{
+    class Chapter2
+    {
+        static void Main(string[] args)
+        {
+            Chapter2 p2 = new Chapter2();
+            
+            p2.Generics();
+            p2.Nullables();
+            p2.Delegates();
+            p2.Yields();
+        }
+
+        /// <summary>
+        /// Test generics
+        /// </summary>
+        private void Generics()
+        {
+            Console.WriteLine("***GENERICS***");
+
+            TestGeneric<int> intGeneric = new TestGeneric<int>();
+            var stringGeneric = new TestGeneric<string>();            
+
+            intGeneric.Increment();
+            stringGeneric.Increment();
+            intGeneric.Increment();
+
+            intGeneric.Display();
+            stringGeneric.Display();
+
+            var teamsList = GetTeams();
+            teamsList.Add("Inter Milan");
+            PrintTeams(teamsList);
+            Console.WriteLine($"String default value within square brackets: [{ default(string) }]");
+            Console.WriteLine($"Integer default value within square brackets: [{ default(int) }]");
+        }
+
+        private static List<string> GetTeams()
+        {
+            List<string> teams = new List<string>();
+            teams.Add("Manchester United");
+            teams.Add("Kobenhavn");
+
+            return teams;
+        }
+
+        private static void PrintTeams(List<string> teams)
+        {            
+            foreach (var team in teams)
+            {                
+                Console.WriteLine(team);
+            }
+        }
+
+        /// <summary>
+        /// Test nullables
+        /// </summary>
+        private void Nullables()
+        {
+            Console.WriteLine("\n***NULLABLES***");
+
+            PrintNullIntCheck(0, null);
+
+            PrintNullIntCheck(1, 2);
+        }
+
+        private static void PrintNullIntCheck(int notNull, int? nullInt)
+        {
+            if (nullInt.HasValue)
+            {
+                Console.WriteLine($"{nameof(nullInt)} value is {nullInt.Value}");
+            }
+            else
+            {
+                Console.WriteLine($"{nameof(nullInt)} value is null");
+            }
+
+            Console.WriteLine($"{nameof(notNull)} value is {notNull}");
+        }
+
+        /// <summary>
+        /// Test delegates
+        /// </summary>
+        private void Delegates()
+        {
+            Console.WriteLine("\n***DELEGATES***");
+
+            string printDelegate = "Printing a string from a delegate";
+
+            StringPrinter stringPrinter = PrintAnything;
+            stringPrinter(printDelegate);
+        }
+
+        public delegate void StringPrinter(string message);
+        public void PrintAnything(Object obj)
+        {
+            Console.WriteLine(obj);
+        }
+
+        private void Yields()
+        {            
+            Console.WriteLine("\n***YIELDS***");            
+
+            foreach (var value in Iterator())
+            {
+                Console.WriteLine($"Returned value from Iterator: { value }");
+            }
+        }
+
+        static IEnumerable<string> Iterator()
+        {
+            try
+            {
+                Console.WriteLine("Before first yield");
+                yield return "first";
+                Console.WriteLine("Between yields");
+                yield return "second";
+                Console.WriteLine("After second yield");
+            }
+            finally
+            {                
+                Console.WriteLine("Finally block");
+            }
+        }
+    }
+
+    class TestGeneric<T>
+    {
+        private int value;
+
+        void GenericCounter()
+        {
+            Console.WriteLine($"Init counter for { typeof(T) }");
+        }
+
+        public void Increment()
+        {
+            value++;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Counter for { typeof(T) }: {value}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+
+  <h3 align="center">CSharpInDepth</h3>
+
+  <p align="center">
+    C# in Depth
+    <br />
+    <br />
+    <a href="https://github.com/halldorstefans/McLarenAPI/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/halldorstefans/McLarenAPI/issues">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#CSharp-In-Depth)
+  * [Built With](#built-with)
+* [License](#license)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
+
+<!-- ABOUT THE PROJECT -->
+## CSharp In Depth
+
+Exploring C# features from 'C# in Depth' by Jon Skeet.
+
+### Built With
+
+* [.NET Core](https://docs.microsoft.com/en-us/dotnet/core/)
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- CONTACT -->
+## Contact
+
+Halldór Stefánsson - [@halldorstefans](https://twitter.com/halldorstefans)
+
+Project Link: [https://github.com/halldorstefans/McLarenAPI](https://github.com/halldorstefans/McLarenAPI)
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+
+* [C# in Depth](https://csharpindepth.com/)
